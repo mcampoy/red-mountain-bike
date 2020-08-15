6c637c309439cb43ad5aa12cbc27f8fd581c4324
Matías Campoy <matiascampoy@gmail.com>
2020-08-15 16:03:55 -0300

    setup del proyecto y Home

---
+++ public/javascripts/map.js
@@ -0,0 +1,9 @@
+var map = L.map('main_map').setView([-32.9668371,-68.890004], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([-32.9655714,-68.8912686]).addTo(map)
+L.marker([-32.9722849,-68.8825144]).addTo(map)
+L.marker([-32.9557964,-68.8591285]).addTo(map)
