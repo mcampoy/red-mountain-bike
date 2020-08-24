e6332dfd9a6261be1a7c18bec58052e0707eec89
Matías Campoy <matiascampoy@gmail.com>
2020-08-24 09:13:15 -0300

    Se incorporó bcrypt al modelo de usuario y el modelo Token

--- database/models/Usuario.js
+++ database/models/Usuario.js
@@ -1,10 +1,52 @@
 const mongoose = require('mongoose');
 const Reserva = require('./Reserva')
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const saltRound = 10;
+
+const validateEmail = function (email) {
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
+};
 
 let usuarioSchema = new Schema({
-    nombre: String
+    nombre: {
+        type: String,
+        trim: true,
+        required: [true, 'El nombre es obligatorio']
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: [true, 'El email es obligatorio'],
+        lowercase: true,
+        validate: [validateEmail, 'Por favor ingresar un email válido'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+    },
+    password: {
+        type: String,
+        required: [true, 'El password es obligatorio']
+    },
+    passwordResetToken: String,
+    passwordResetTokenExpire: Date,
+    verificado: {
+        type: Boolean,
+        default: false
+    }
 });
+
+// usuarioSchema.plugin(uniqueValidator, { message: 'El {PATH} ya existe con otro usuario'});
+
+usuarioSchema.pre('save', function(next){
+    if(this.isModified('password')){
+        this.password = bcrypt.hashSync(this.password, saltRound);
+    }
+    next();
+});
+
+usuarioSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+};
 
 
 usuarioSchema.methods.reservar = function(biciID, desde, hasta, cb){
